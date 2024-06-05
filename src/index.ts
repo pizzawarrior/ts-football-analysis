418f1ffb669064f2e5d3416445ed1cd497618bb3
pizzawarrior <i.norstad@gmail.com>
2024-06-04 21:42:23 -0700

    basic file read function and test analysis

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('hello there')
+import fs, { readFileSync } from 'fs';
+
+const matches = fs
+.readFileSync('football.csv', {
+    encoding: "utf-8"
+})
+    .trim()
+    .split('\n')
+    .map((row: string): string[] => {
+        return row.split(',')
+    })
+
+// test analyze data:
+const manUnitedWins = (games: string[][]) => {
+    let totalWins: number = 0;
+    for (let game of games) {
+        if (game[1] === 'Man United' && game[5] === 'H' || game[2] === 'Man United' && game[5] === 'A') {
+            totalWins++;
+        }
+    }
+    return totalWins;
+}
+
+console.log(manUnitedWins(matches))
