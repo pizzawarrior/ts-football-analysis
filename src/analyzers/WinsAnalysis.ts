f3656c279c2eefc3b2d5782d12ac969b705a0307
pizzawarrior <i.norstad@gmail.com>
2024-06-12 14:38:07 -0700

    tie all classes together and generate reports

--- src/analyzers/WinsAnalysis.ts
+++ src/analyzers/WinsAnalysis.ts
@@ -3,10 +3,9 @@ import { FormatDataRow } from "../FormatDataRow";
 import { MatchResult } from "../MatchResult";
 
 export class WinsAnalysis implements Analyzer {
-    constructor(public teamName: string) {}
+    constructor(public teamName: string = 'Man United') {}
 
     run(matches: FormatDataRow[]): string {
-        this.teamName = 'Man United';
         let totalWins: number = 0;
         for (let game of matches) {
             if (game[1] === this.teamName && game[5] === MatchResult.HomeTeamWon || game[2] === this.teamName && game[5] === MatchResult.AwayTeamWon) {
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import { MatchResult } from "./MatchResult";
 import { MatchReader } from "./MatchReader";
 import { CsvFileReader } from "./CsvFileReader";
+import { WinsAnalysis } from "./analyzers/WinsAnalysis";
+import { AverageGoalsAnalysis } from "./analyzers/AverageGoalsAnalysis";
+import { ConsoleReport } from "./reportTargets/ConsoleReport";
+import { GenerateSummary } from "./GenerateSummary";
+
 
 let csvFileName = 'football.csv';
 
@@ -10,3 +14,13 @@ const csvFileReader = new CsvFileReader(csvFileName);
 // create an instance of MatchReader and pass in the object that satisfies the DataReader interface
 const newMatch = new MatchReader(csvFileReader);
 newMatch.load();
+
+// create instances of all classes and pass in required objects
+const winsAnalysis = new WinsAnalysis()
+const averageGoalsAnalysis = new AverageGoalsAnalysis()
+const consoleReport = new ConsoleReport()
+const generateWinsSummary = new GenerateSummary(winsAnalysis, consoleReport)
+const generateAverageScorePerGameSummary = new GenerateSummary(averageGoalsAnalysis, consoleReport)
+
+generateWinsSummary.buildAndPrintReport(newMatch.matches)
+generateAverageScorePerGameSummary.buildAndPrintReport(newMatch.matches)
