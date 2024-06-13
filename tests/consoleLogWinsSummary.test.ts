b2b3c79f112633bb786c96c07ee952be1a9f8bcb
pizzawarrior <i.norstad@gmail.com>
2024-06-13 09:02:00 -0700

    added a test for MatchReader class

--- tests/consoleLogWinsSummary.test.ts
+++ tests/consoleLogWinsSummary.test.ts
@@ -4,6 +4,7 @@ import { FormatDataRow } from "../src/FormatDataRow";
 import { dateStringtoDate } from "../src/utils";
 import fs from 'fs';
 import { GenerateSummary } from "../src/GenerateSummary";
+import { match } from "assert";
 
 jest.mock("../src/GenerateSummary");
 jest.mock('fs');
@@ -16,15 +17,25 @@ describe("static function named 'consoleLogWinsSummary'", () => {
     });
 })
 
-
 describe('CsvFileReader', () => {
     it('reads and parses CSV files correctly', () => {
-        const fileContent = '12/06/2024,Man United,Chelsea,2,1,H,Smith';
+        const fileContent = '12/06/2024,Man United,Chelsea,2,1,H,N Smith';
         jest.spyOn(fs, 'readFileSync').mockReturnValueOnce(fileContent);
 
         const csvFileReader = new CsvFileReader('dummy.csv');
         csvFileReader.read();
 
-        expect(csvFileReader.data).toEqual([['12/06/2024', 'Man United', 'Chelsea', '2', '1', 'H', 'Smith']])
+        expect(csvFileReader.data).toEqual([['12/06/2024', 'Man United', 'Chelsea', '2', '1', 'H', 'N Smith']])
     });
 });
+
+describe('MatchReader', () => {
+    it('loads and processes match data correctly', () => {
+        const fileContent = '12/06/2024,Man United,Chelsea,2,1,H,N Smith';
+        jest.spyOn(fs, 'readFileSync').mockImplementationOnce(() => fileContent);
+
+        const matchReader = MatchReader.newMatch('dummy.csv');
+        matchReader.load();
+        expect(matchReader.matches).toEqual([[new Date(2024, 5, 12), 'Man United', 'Chelsea', 2, 1, 'H', 'N Smith']])
+    })
+})
