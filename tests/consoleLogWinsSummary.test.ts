97486b11e8c62cd6f4b7d9657d392b9fdb5efc9b
pizzawarrior <i.norstad@gmail.com>
2024-06-13 11:23:52 -0700

    more GenerateSummaries tests, consoleLogAverageScorePerGame static method

--- tests/consoleLogWinsSummary.test.ts
+++ tests/consoleLogWinsSummary.test.ts
@@ -6,6 +6,7 @@ import { dateStringtoDate } from "../src/utils";
 import { GenerateSummary } from "../src/GenerateSummary";
 import { Analyzer, OutputTarget } from '../src/GenerateSummary';
 import { WinsAnalysis } from '../src/analyzers/WinsAnalysis';
+import { AverageGoalsAnalysis } from '../src/analyzers/AverageGoalsAnalysis';
 import { HtmlReport } from '../src/reportTargets/HtmlReport';
 import { ConsoleReport } from '../src/reportTargets/ConsoleReport';
 import { MatchResult } from '../src/MatchResult';
@@ -53,7 +54,6 @@ describe('Generate Summary', () => {
         ];
 
         const consoleSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
-
         const MockedGenerateSummary = GenerateSummary as jest.Mocked<typeof GenerateSummary>;
 
         const mockInstance = new MockedGenerateSummary(new WinsAnalysis('Man United'), new ConsoleReport());
@@ -61,6 +61,27 @@ describe('Generate Summary', () => {
         MockedGenerateSummary.consoleLogWinsSummary.mockReturnValue(mockInstance);
 
         const summary = GenerateSummary.consoleLogWinsSummary('Man United');
+        summary.buildAndPrintReport(matches);
+
+        expect(summary.buildAndPrintReport).toHaveBeenCalledWith(matches);
+        consoleSpy.mockRestore();
+    })
+
+    it('generates and prints the average-score-per-game report correctly', () => {
+        const matches: FormatDataRow[] = [
+            [new Date(2024, 5, 28), 'Man United', 'Chelsea', 2, 1, MatchResult.HomeTeamWon, 'N Smith'],
+            [new Date(2024, 5, 25), 'Chelsea', 'Man United', 1, 2, MatchResult.AwayTeamWon, 'T Jones'],
+            [new Date(2024, 5, 30), 'Man United', 'Liverpool', 0, 0, MatchResult.Draw, 'M Taylor']
+        ]
+
+        const consoleSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
+        const MockedGenerateSummary = GenerateSummary as jest.Mocked<typeof GenerateSummary>;
+
+        const mockInstance = new MockedGenerateSummary(new AverageGoalsAnalysis('Man United'), new ConsoleReport());
+        mockInstance.buildAndPrintReport = jest.fn();
+        MockedGenerateSummary.consoleLogAverageScorePerGameSummary.mockReturnValue(mockInstance);
+
+        const summary = GenerateSummary.consoleLogAverageScorePerGameSummary('Man United');
         summary.buildAndPrintReport(matches);
 
         expect(summary.buildAndPrintReport).toHaveBeenCalledWith(matches);
